5097545482852d7b05471b1c47bc55d4f937300a
YoussefJoker1 <111king.yousef@gmail.com>
2026-01-16 01:22:53 +0200

    Update nav-active.js

--- nav-active.js
+++ nav-active.js
@@ -1,4 +1,8 @@
-const currentPage = window.location.pathname.split("/").pop();
+let currentPage = window.location.pathname.split("/").pop();
+
+if (currentPage === "") {
+  currentPage = "index.html";
+}
 
 document.querySelectorAll(".main-nav a").forEach(link => {
   if (link.getAttribute("href") === currentPage) {
